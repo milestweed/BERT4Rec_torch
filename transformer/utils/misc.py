cd8383d494ea14fa32d20f199b94c870c5ed8fc1
Miles Tweed <milestweed@gmail.com>
2023-07-11 15:22:42 -0400

    added mask utility

--- transformer/utils/misc.py
+++ transformer/utils/misc.py
@@ -1,7 +1,16 @@
 import copy
 
+import torch
 import torch.nn as nn
 
-def clones(module, N):
+
+def clones(module, N: int) -> nn.ModuleList:
     "Produce N identical layers"
     return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+
+def subsequent_mask(size: int) -> torch.Tensor:
+    "Mask out subsequent position"
+    attn_shape = (1, size, size)
+    subsequent_mask = torch.triu(torch.ones(attn_shape), diagonal=1).type(torch.uint8)
+    return subsequent_mask == 0
