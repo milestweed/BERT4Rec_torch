95dd35aef5c0157379054f3b7f6a6a987be821b1
Miles Tweed <milestweed@gmail.com>
2023-07-11 22:34:01 -0400

    transformer implementation

--- transformer/encoder-decoder/__init__.py
+++
@@ -1,2 +0,0 @@
-from .decoder import Decoder, DecoderLayer
-from .encoder import Encoder, EncoderLayer
--- transformer/utils/__init__.py
+++ transformer/utils/__init__.py
@@ -1,3 +1,5 @@
 from layer_norm import LayerNorm
 from sublayer import SublayerConnection
+from feed_forward import PositionwiseFeedForward
 from misc import clones
+from embeddings import SinusoidalPositionEncoding, Embeddings
