f2ba1e527f0631da68c4407cd2887bad15d0353e
Miles Tweed <milestweed@gmail.com>
2023-07-27 14:10:01 -0400

    init gitignore update

--- transformer/utils/__init__.py
+++ transformer/utils/__init__.py
@@ -1,5 +1,5 @@
-from layer_norm import LayerNorm
-from sublayer import SublayerConnection
-from feed_forward import PositionwiseFeedForward
-from misc import clones
-from embeddings import SinusoidalPositionEncoding, Embeddings
+from .layer_norm import LayerNorm
+from .sublayer import SublayerConnection
+from .feed_forward import PositionwiseFeedForward
+from .misc import clones
+from .embeddings import SinusoidalPositionEncoding, Embeddings
